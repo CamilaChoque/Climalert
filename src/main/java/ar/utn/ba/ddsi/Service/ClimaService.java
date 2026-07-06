a56ca19e691820cbd3bd4c890194fbae5fca3d39
CamilaChoque <camilaichoquem@gmail.com>
2026-07-06 13:59:53 -0300

    Nuevo: credenciales

--- src/main/java/ar/utn/ba/ddsi/Service/ClimaService.java
+++ src/main/java/ar/utn/ba/ddsi/Service/ClimaService.java
@@ -1,4 +1,7 @@
 package ar.utn.ba.ddsi.Service;
 
+import ar.utn.ba.ddsi.models.RegistroClimatico;
+
 public interface ClimaService {
+    RegistroClimatico obtenerClimaActual();
 }
---
+++ src/main/java/ar/utn/ba/ddsi/Service/ClimaServiceImpl.java
@@ -0,0 +1,11 @@
+package ar.utn.ba.ddsi.Service;
+
+import ar.utn.ba.ddsi.models.RegistroClimatico;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClimaServiceImpl implements ClimaService{
+    private
+    public RegistroClimatico obtenerClimaActual(){
+    }
+}
