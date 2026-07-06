c17f54958d577ac9b961eb784f5c3addf81972a6
CamilaChoque <camilaichoquem@gmail.com>
2026-07-06 15:29:40 -0300

    Nuevo: obtenerUltimo en inMemoryClimaRepository

--- src/main/java/ar/utn/ba/ddsi/repositories/ClimaRepository.java
+++ src/main/java/ar/utn/ba/ddsi/repositories/ClimaRepository.java
@@ -5,6 +5,7 @@ import ar.utn.ba.ddsi.models.RegistroClimatico;
 public interface ClimaRepository {
 
     RegistroClimatico guardar(RegistroClimatico camion);
+
     RegistroClimatico obtenerUltimo();
 
 }
--- src/main/java/ar/utn/ba/ddsi/repositories/inMemory/InMemoryClimaRepository.java
+++ src/main/java/ar/utn/ba/ddsi/repositories/inMemory/InMemoryClimaRepository.java
@@ -1,6 +1,24 @@
 package ar.utn.ba.ddsi.repositories.inMemory;
 
+import ar.utn.ba.ddsi.models.RegistroClimatico;
 import ar.utn.ba.ddsi.repositories.ClimaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class InMemoryClimaRepository implements ClimaRepository {
+    private final ClimaInMemoryStore store;
+
+    public InMemoryClimaRepository(ClimaInMemoryStore store) {
+        this.store = store;
+    }
+    @Override
+    public RegistroClimatico guardar(RegistroClimatico registro){
+        RegistroClimatico guardado = store.guardar(registro);
+        return guardado;
+    }
+    @Override
+    public RegistroClimatico obtenerUltimo(){
+        RegistroClimatico obtenido = store.obtenerUltimo();
+        return obtenido;
+    }
 }
