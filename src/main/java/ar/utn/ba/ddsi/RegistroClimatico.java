f4a60571cc1f48a00566135e18c492cfb43f0dc5
CamilaChoque <camilaichoquem@gmail.com>
2026-07-06 12:04:17 -0300

    Nuevo: estructuras

---
+++ src/main/java/ar/utn/ba/ddsi/RegistroClimatico.java
@@ -0,0 +1,9 @@
+package ar.utn.ba.ddsi;
+
+import java.time.LocalDateTime;
+
+public class RegistroClimatico {
+    private LocalDateTime fecha;
+    private double temperatura;
+    private double humedad;
+}
---
+++ src/main/java/ar/utn/ba/ddsi/TipoAlerta.java
@@ -0,0 +1,5 @@
+package ar.utn.ba.ddsi;
+
+public enum TipoAlerta {
+
+}
